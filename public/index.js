24a9fe502432eddf80bed777cd679496dd7a480a
SteveAmCoden <smkhitar@smail.uni-koeln.de>
2023-04-28 19:59:30 +0200

    barchart added

--- public/index.js
+++ public/index.js
@@ -1,20 +1,124 @@
 function print_hello_world() {
-  console.log("Hello world!")
+  console.log("Hello world!");
 }
 // this is working because of the import in the html file
 // https://socket.io/docs/v4/client -installation/#standalone -build
-const socket = io()
+const socket = io();
 
 socket.on("connect", () => {
-  console.log("Connected to the webserver.")
-})
+  console.log("Connected to the webserver.");
+});
 socket.on("disconnect", () => {
-  console.log("Disconnected from the webserver.")
-})
+  console.log("Disconnected from the webserver.");
+});
 socket.on("example_data", (obj) => {
-  console.log(obj)
-})
+  console.log(obj);
+});
+
+function getUniqueMinimumAges(dataFile) {
+  console.log(typeof dataFile);
+  console.log(dataFile);
+}
+
+function createBarChart(data) {
+  // Set up the scales for the chart
+  const xScale = d3
+    .scaleBand()
+    .domain(data.map((d) => d.minAge))
+    .range([0, 400])
+    .padding(0.1);
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, (d) => d.frequency)])
+    .range([350, 0]);
+
+  // Set up the axes for the chart
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  // Add the bars to the chart
+  const svg = d3.select("svg");
+  const bars = svg
+    .selectAll("rect")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("x", (d) => xScale(d.minAge))
+    .attr("y", (d) => yScale(d.frequency))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => 350 - yScale(d.frequency))
+    .attr("fill", "steelblue");
+
+  // Add the x-axis to the chart
+  svg.append("g").attr("transform", "translate(0, 350)").call(xAxis);
+
+  // Add the y-axis to the chart
+  svg.append("g").call(yAxis);
+}
+
+function createBarChart2(dataFile) {
+  // socket.emit("get_data", string);
+
+  // Define the dimensions and margins of the chart
+  const margin = { top: 20, right: 20, bottom: 50, left: 50 };
+  const width = 600 - margin.left - margin.right;
+  const height = 400 - margin.top - margin.bottom;
+
+  // Append the SVG element to the body of the page
+  const svg = d3
+    .select("body")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Load the data from the JSON file
+  d3.json(dataFile).then(function (data) {
+    // Define the x and y scales and axes
+    const xScale = d3.scaleBand().range([0, width]).padding(0.1);
+    const yScale = d3.scaleLinear().range([height, 0]);
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    // Map the data to the x and y domains
+    xScale.domain(data.map((d) => d.variable1));
+    yScale.domain([0, d3.max(data, (d) => d.variable2)]);
+
+    // Append the x and y axes to the chart
+    svg
+      .append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+      .selectAll("text")
+      .attr("transform", "rotate(-45)")
+      .style("text-anchor", "end");
+    svg.append("g").call(yAxis);
+
+    // Append the bars to the chart
+    svg
+      .selectAll(".bar")
+      .data(data)
+      .enter()
+      .append("rect")
+      .attr("class", "bar")
+      .attr("x", (d) => xScale(d.variable1))
+      .attr("y", (d) => yScale(d.variable2))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d) => height - yScale(d.variable2))
+      .attr("fill", "steelblue");
+  });
+}
 
 function request_example_data(string) {
-  socket.emit("get_data", string)
+  socket.emit("get_data", null);
 }
+function getBoardGameData() {
+  socket.emit("get_data", "boardgames_40");
+}
+
+socket.on("receiveData", (data) => {
+  console.log(data);
+  createBarChart(data);
+});
--- server.js
+++ server.js
@@ -1,39 +1,54 @@
-const fs = require("fs")
+const fs = require("fs");
 
-const port = 8080
+const port = 8080;
 
-const express = require("express")
-const app = express()
-app.use(express.static("public"))
-const server = app.listen(port)
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const server = app.listen(port);
 
-console.log(`Webserver is running on port ${port}.`)
+console.log(`Webserver is running on port ${port}.`);
 
-const socket = require("socket.io")
-const io = socket(server)
+const socket = require("socket.io");
+const io = socket(server);
 io.sockets.on("connection", (socket) => {
-    console.log(`Client ${socket.id} connected.`)
-    
-    let disconnect = () => {
-        console.log(`Client ${socket.id} disconnected.`)
-    }
-    
+  console.log(`Client ${socket.id} connected.`);
 
-    
-    let get_data = (parameters) => {
-        console.log(`Received data request with these parameters: ${parameters}`)
-        fs.readFile(`./data/${parameters}.json`, "utf8", (err, data) => {
-        if (err) {
-        console.error(err)
-        return
+  let disconnect = () => {
+    console.log(`Client ${socket.id} disconnected.`);
+  };
+
+  let get_data = (parameters) => {
+    console.log(`Received data request with these parameters: ${parameters}`);
+    fs.readFile(`./data/${parameters}.json`, "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      let json_data = JSON.parse(data);
+      console.log(json_data);
+
+      var dict = {};
+
+      for (i = 0; i < json_data.length; i++) {
+        // json_data[i]["minage"];
+        var currentEntry = json_data[i]["minage"];
+        if (dict[currentEntry] === undefined) {
+          dict[currentEntry] = 1;
+        } else {
+          dict[currentEntry] = dict[currentEntry] + 1;
         }
-        let json_data = JSON.parse(data)
-        socket.emit("example_data", json_data)
-        })
-        }
-        
+      }
+      console.log(dict);
+      var arr = [];
+      for (const [key, value] of Object.entries(dict)) {
+        arr.push({ minAge: key, frequency: value });
+      }
+      console.log(arr);
+      socket.emit("receiveData", arr);
+    });
+  };
 
-    socket.on("disconnect", disconnect)
-
-    socket.on("get_data", get_data)
-})
+  socket.on("disconnect", disconnect);
+  socket.on("get_data", get_data);
+});
